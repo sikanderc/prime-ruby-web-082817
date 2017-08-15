d7fe483983d689bae441a5007239fa1e94106635
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-08-15 21:14:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  i = 2
+  if number < 2
+    return false
+  end
+  until i == number
+    while number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
